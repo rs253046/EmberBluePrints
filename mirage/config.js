c191a02f9664f6dd64f8ed8b89abe240e0a1a0d5
Rahul <rahulsept2191@gmail.com>
2016-10-22 21:27:55 +0530

    setup mirage db

--- mirage/config.js
+++ mirage/config.js
@@ -1,26 +1,29 @@
-export default function() {
+ export default function() {
+  this.namespace = 'https://localhost:3000/api/v1';
+  let rentals = [{
+    type: 'cruds',
+    id: '1',
+    attributes: {
+      name: 'Grand Old Mansion',
+      class: true
+    }
+  }, {
+    type: 'cruds',
+    id: '2',
+    attributes: {
+      name: 'Urban Living',
+      class: true
+    }
+  }, {
+    type: 'cruds',
+    id: '3',
+    attributes: {
+      name: 'Downtown Charm',
+      class: false
+    }
+  }];
 
-  // These comments are here to help you get started. Feel free to delete them.
-
-  /*
-    Config (with defaults).
-
-    Note: these only affect routes defined *after* them!
-  */
-
-  // this.urlPrefix = '';    // make this `http://localhost:8080`, for example, if your API is on a different server
-  // this.namespace = '';    // make this `api`, for example, if your API is namespaced
-  // this.timing = 400;      // delay for each request, automatically set to 0 during testing
-
-  /*
-    Shorthand cheatsheet:
-
-    this.get('/posts');
-    this.post('/posts');
-    this.get('/posts/:id');
-    this.put('/posts/:id'); // or this.patch
-    this.del('/posts/:id');
-
-    http://www.ember-cli-mirage.com/docs/v0.2.x/shorthands/
-  */
+  this.get('/cruds');
+  this.post('cruds');
+  
 }
